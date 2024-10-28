9f32bba59466547c7cf3a48eef37df75c5df7b35
Aljes <aleksandr.binkevich@gmail.com>
2024-10-28 19:42:27 +0000

    Added warning in case of zero fragments for celltypes

--- bin/make_pseudobulk.py
+++ bin/make_pseudobulk.py
@@ -4,51 +4,12 @@ import os
 import sys
 import logging
 from typing import List
+from colored_logger import setup_logging
 from pandas import read_csv, read_table, DataFrame
 from pandas.errors import InvalidColumnName, DataError
 from pycisTopic.pseudobulk_peak_calling import export_pseudobulk
 
 CELLTYPE_COLUMNS = {"sample_id", "barcode", "celltype"}
-
-
-class ColoredFormatter(logging.Formatter):
-    blue = "\n\033[94m"
-    yellow = "\033[93m"
-    red = "\033[91m"
-    reset = "\033[0m"
-    format = "%(levelname)s: %(message)s"
-
-    FORMATS = {
-        logging.INFO: blue + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-    }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-
-def setup_logging() -> None:
-    """
-    Setup logging configuration of the script
-    """
-    # a basic config to save logs to metadata.log
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(levelname)s: %(message)s",
-        filename=".python.log",
-        filemode="w",
-    )
-
-    # define a Handler which writes INFO messages or higher to the sys.stderr
-    console = logging.StreamHandler(sys.stdout)
-    console.setLevel(logging.INFO)
-    # tell the handler to use colored format
-    console.setFormatter(ColoredFormatter())
-    # add the handler to the root logger
-    logging.getLogger("").addHandler(console)
 
 
 def init_parser() -> argparse.ArgumentParser:
@@ -117,6 +78,16 @@ def init_parser() -> argparse.ArgumentParser:
         default="atac_fragments",
     )
     parser.add_argument(
+        "--dropna",
+        help="If specified drops NaN values from celltype annotation",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--skip_empty_fragments",
+        help="If specified skips celltypes with no fragments found",
+        action="store_true",
+    )
+    parser.add_argument(
         "--cpus", metavar="<num>", type=int, help="Specify a number of cpu cores to use"
     )
     return parser
@@ -156,44 +127,6 @@ def validate_celltype_columns(
         )
 
 
-def vaildate_nan_entries(sample_id: str, celltypes: DataFrame) -> None:
-    """
-    Checks if there are any NaN values in celltypes dataframe for sample_id
-    sample_id (str): sample identifier
-    celltypes (DataFrame): a dataframe with barcode/celltype annotation for sample_id
-    """
-    if celltypes.isna().values.any():
-        raise DataError(
-            f"Cell-type annotation file contains NaN values for sample_id={sample_id}"
-        )
-
-
-def validate_redundunt_celltypes(
-    sample_id: str, celltypes: DataFrame, barcode_metrics: DataFrame, fragments_col: str
-):
-    """
-    Checks if there are celltypes for which there is no barcodes in fragments file
-    sample_id (str): sample identifier
-    celltypes (DataFrame): a dataframe with barcode/celltype annotation for sample_id
-    barcode_metrics (str): a DataFrame with barcode metrics
-    fragments_col str: a name of the column with counts of fragments per barcode
-    """
-    # join two DataFrames
-    joined_df = celltypes.merge(barcode_metrics, left_on="barcode", right_on="barcode")
-    # calculate a total number of fragments for each celltype
-    total_fragments = joined_df.groupby("celltype").sum(numeric_only=True)
-    total_fragments.to_csv(f"{sample_id}_celltypes_fragment_counts.csv")
-    # get celltypes with zero fragments
-    zero_fragments_celltypes = total_fragments[
-        total_fragments[fragments_col] == 0
-    ].index.to_list()
-    if zero_fragments_celltypes:
-        celltypes_string = ",".join(zero_fragments_celltypes)
-        raise ValueError(
-            f"Sample {sample_id} containts zero fragments for the following celltypes: {celltypes_string}"
-        )
-
-
 def read_celltype_annotation(sample_id: str, celltype_file: str) -> DataFrame:
     """
     Read cell-type annotation file to pandas DataFrame
@@ -209,6 +142,115 @@ def read_celltype_annotation(sample_id: str, celltype_file: str) -> DataFrame:
     return celltypes
 
 
+def filter_nan_entries(sample_id: str, celltypes: DataFrame, dropna: bool) -> None:
+    """
+    Checks if there are any NaN values in celltypes dataframe for sample_id
+    sample_id (str): sample identifier
+    celltypes (DataFrame): a dataframe with barcode/celltype annotation for sample_id
+    """
+    nan_etries = celltypes.isna().values.any()
+    if nan_etries:
+        if dropna:
+            logging.warning(
+                f"Cell-type annotation file contains NaN values for sample_id={sample_id}"
+            )
+            logging.info(
+                f"Removing NaN values from celltype annotation for {sample_id}"
+            )
+            celltypes.dropna(inplace=True)
+        else:
+            raise DataError(
+                f"Cell-type annotation file contains NaN values for sample_id={sample_id}"
+            )
+
+
+def find_zero_fragment_celltypes(
+    sample_id: str,
+    celltypes: DataFrame,
+    barcode_metrics: DataFrame,
+    celltype_col: str,
+    fragments_col: str,
+) -> List[str]:
+    """
+    Find all celltypes for which there are no fragments found
+    sample_id (str): sample identifier
+    celltypes (DataFrame): a dataframe with barcode/celltype annotation for sample_id
+    barcode_metrics (str): a DataFrame with barcode metrics
+    celltype_col (str): a name of the column with celltype labels in celltypes DataFrame
+    fragments_col str: a name of the column with counts of fragments per barcode
+    """
+    # join two DataFrames
+    joined_df = celltypes.merge(barcode_metrics, left_on="barcode", right_on="barcode")
+    # calculate a total number of fragments for each celltype
+    total_fragments = joined_df.groupby(celltype_col).sum(numeric_only=True)
+    total_fragments.to_csv(f"{sample_id}_celltypes_fragment_counts.csv")
+    # get celltypes with zero fragments
+    zero_fragments_celltypes = total_fragments[
+        total_fragments[fragments_col] == 0
+    ].index.to_list()
+    return zero_fragments_celltypes
+
+
+def drop_empty_fragment(
+    sample_id: str,
+    celltypes: DataFrame,
+    zero_fragments_celltypes: List[str],
+    celltype_col: str,
+):
+    """
+    Drops celltypes with zero fragments from celltype DataFrame
+    sample_id (str): sample identifier
+    celltypes (DataFrame): a dataframe with barcode/celltype annotation for sample_id
+    zero_fragments_celltypes (List[str]): a list of celltypes with zero fragments
+    celltype_col (str): a name of the column with celltype labels in celltypes DataFrame
+    """
+    # log this step
+    celltype_string = celltypes_string = ",".join(zero_fragments_celltypes)
+    logging.info(
+        f"Dropping the following celltypes from the {sample_id}'s annotation DataFrame as no fragments where found: {celltypes_string}"
+    )
+    # drop the celltypes
+    zero_fragment_cells = celltypes[
+        celltypes[celltype_col].isin(zero_fragments_celltypes)
+    ].index
+    celltypes.drop(zero_fragment_cells, inplace=True)
+
+
+def filter_empty_fragments(
+    sample_id: str,
+    celltypes: DataFrame,
+    barcode_metrics: DataFrame,
+    celltype_col: str,
+    fragments_col: str,
+    skip_empty_fragments: bool,
+) -> DataFrame:
+    """
+    Filter celltypes with zero fragments
+    sample_id (str): sample identifier
+    celltypes (DataFrame): a dataframe with barcode/celltype annotation for sample_id
+    barcode_metrics (str): a DataFrame with barcode metrics
+    celltype_col (str): a name of the column with celltype labels in celltypes DataFrame
+    fragments_col str: a name of the column with counts of fragments per barcode
+    skip_empty_fragments: if true removes all celltypes with zero fragments from DataFrame
+    """
+    zero_fragments_celltypes = find_zero_fragment_celltypes(
+        sample_id, celltypes, barcode_metrics, celltype_col, fragments_col
+    )
+    if zero_fragments_celltypes:
+        celltypes_string = ",".join(zero_fragments_celltypes)
+        if skip_empty_fragments:
+            logging.warning(
+                f"Sample {sample_id} containts zero fragments for the following celltypes: {celltypes_string}"
+            )
+            drop_empty_fragment(
+                sample_id, celltypes, zero_fragments_celltypes, celltype_col
+            )
+        else:
+            raise ValueError(
+                f"Sample {sample_id} containts zero fragments for the following celltypes: {celltypes_string}"
+            )
+
+
 def main():
     # set up logger
     setup_logging()
@@ -222,10 +264,15 @@ def main():
     celltypes = read_celltype_annotation(args.sample_id, args.celltype_annotation)
     barcode_metrics = read_barcode_metrics(args.barcode_metrics, args.fragments_col)
 
-    # validate celltypes DataFrame
-    vaildate_nan_entries(args.sample_id, celltypes)
-    validate_redundunt_celltypes(
-        args.sample_id, celltypes, barcode_metrics, args.fragments_col
+    # filter celltypes DataFrame
+    filter_nan_entries(args.sample_id, celltypes, args.dropna)
+    filter_empty_fragments(
+        args.sample_id,
+        celltypes,
+        barcode_metrics,
+        args.celltype_col,
+        args.fragments_col,
+        args.skip_empty_fragments,
     )
 
     # get fragments file path dict
