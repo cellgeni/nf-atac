578a52b337a560f5938d7f22b6bace9cf2160541
Aljes <aleksandr.binkevich@gmail.com>
2024-11-21 15:41:52 +0000

    Added fragment counts calculation to the split annotation step

--- bin/split_annotation.py
+++ bin/split_annotation.py
@@ -3,8 +3,9 @@
 import os
 import logging
 import argparse
-from typing import List, Set
-from pandas import read_csv, DataFrame
+import collections
+from typing import List, Dict
+from pandas import read_csv, concat, DataFrame, Series
 from colored_logger import setup_logging
 from pandas.errors import InvalidColumnName, DataError
 
@@ -17,16 +18,24 @@ def init_parser() -> argparse.ArgumentParser:
         description="Script validates sample and annotation tables and splits annotation table into separate celltypes"
     )
     parser.add_argument(
-        "--sample_table",
-        metavar="<file>",
+        "--sample_id",
         type=str,
-        help="Specify a path to the sample file",
+        metavar="<val>",
+        nargs="+",
+        help="Specify sample ids in celltype annotation file you want to process",
     )
     parser.add_argument(
         "--celltype_annotation",
         metavar="<file>",
         type=str,
         help="Specify a path to the cell-type annotation file",
+    )
+    parser.add_argument(
+        "--barcode_metrics",
+        type=str,
+        metavar="<file>",
+        nargs="+",
+        help="Specify Cellranger's per_barcode_metrics.csv files in the same order as sample ids",
     )
     parser.add_argument(
         "--output_dir",
@@ -64,6 +73,13 @@ def init_parser() -> argparse.ArgumentParser:
         default="barcode",
     )
     parser.add_argument(
+        "--fragments_col",
+        metavar="<val>",
+        type=str,
+        help="Specify a name for fragments column in 'per_barcode_metrics.csv' file",
+        default="atac_fragments",
+    )
+    parser.add_argument(
         "--filedir_col",
         metavar="<val>",
         type=str,
@@ -76,8 +92,8 @@ def init_parser() -> argparse.ArgumentParser:
         action="store_true",
     )
     parser.add_argument(
-        "--drop_duplicated_samples",
-        help="If specified drops duplicated samples from sample table",
+        "--skip_empty_fragments",
+        help="If specified skips celltypes with no fragments found",
         action="store_true",
     )
     parser.add_argument(
@@ -87,85 +103,67 @@ def init_parser() -> argparse.ArgumentParser:
         help="Specify a log file name",
         default="splitcelltypes.log",
     )
+    parser.add_argument(
+        "--fragments_celltype_x_sample",
+        metavar="<file>",
+        type=str,
+        help="Specify a name for the fragments_celltype_x_sample file",
+        default="fragments_celltype_x_sample.csv",
+    )
+    parser.add_argument(
+        "--fragments_per_celltype",
+        metavar="<file>",
+        type=str,
+        help="Specify a name for the fragments_per_celltype file",
+        default="fragments_per_celltype.csv",
+    )
+    parser.add_argument(
+        "--fragments_per_sample",
+        metavar="<file>",
+        type=str,
+        help="Specify a name for the fragments_per_sample file",
+        default="fragments_per_sample.csv",
+    )
     return parser
 
 
-def validate_sample_table_columns(
-    sample_table_columns: List[str], sample_id_col: str, filedir_col: str
-) -> None:
+def validate_sample_list(samples: List[str]) -> None:
     """
-    Check if sample dataframe contains all necessary columns
-    sample_table_columns (List[str]): a list of columns in sample table
-    sample_id_col (str): a name for sample_id column in sample files
-    filedir_col (str): a name for cellranger-arc output directory column in annotation and sample files
-    """
-    reference_columns = [sample_id_col, filedir_col]
-    if reference_columns != sample_table_columns:
-        raise InvalidColumnName(
-            "Sample table file must have '{sample_id_col}' and '{filedir_col}' columns"
-        )
+    Check if there are duplicated samples in the sample list
 
-
-def drops_duplicated_sample_ids(
-    sample_df: DataFrame, sample_id_col: str, drop_duplicated_samples: bool
-) -> DataFrame:
+    Args:
+        samples (List[str]): a list of sample ids
+    Raises:
+        DataError: if there is duplicates in the list of samples
     """
-    Remove dupicated sample ids
-    sample_df (DataFrame): a DataFrame with sample table
-    sample_id_col (str): a name for sample_id column in asample file
-    drop_duplicated_samples (bool): if true drops duplicated samples from sample table
-    """
-    # find duplicated samples
-    duplicated = sample_df[sample_id_col].duplicated(keep="first")
-    if duplicated.any() and drop_duplicated_samples:
-        # raise warning
-        dup_samples_string = ",".join(
-            sample_df[duplicated]["sample_id_col"].unique().tolist()
+    if len(samples) != len(set(samples)):
+        duplicated_samples = [
+            sample
+            for sample, count in collections.Counter(samples).items()
+            if count > 1
+        ]
+        duplicated_samples_string = ",".join(duplicated_samples)
+        raise DataError(
+            f"Multiple entries of {duplicated_samples_string} in sample table"
         )
-        logging.warning(
-            f"Multiple entries of {dup_samples_string} in sample table. Dropping duplicated..."
-        )
-        # drop duplicated samples
-        sample_df_filtered = sample_df[~duplicated].copy()
-        return sample_df_filtered
-    elif duplicated.any() and not drop_duplicated_samples:
-        # raise an error
-        dup_samples_string = ",".join(
-            sample_df[duplicated]["sample_id_col"].unique().tolist()
-        )
-        raise DataError(f"Multiple entries of {dup_samples_string} in sample table")
-    return sample_df
-
-
-def read_sample_table(
-    filepath: str, sample_id_col: str, filedir_col: str, drop_duplicated_samples: bool
-) -> DataFrame:
-    """
-    Read and validate sample table
-    filepath (str): a path to sample table
-    sample_id_col (str): a name for sample_id column in sample files
-    filedir_col (str): a name for cellranger-arc output directory column in annotation and sample files
-    drop_duplicated_samples (bool): if true drops duplicated samples from sample table
-    """
-    # read to DataFrame
-    sample_df = read_csv(filepath)
-    # check if column names are valid
-    df_columns = sample_df.columns.to_list()
-    validate_sample_table_columns(df_columns, sample_id_col, filedir_col)
-    # remove duplicated sample ids
-    sample_df = drops_duplicated_sample_ids(
-        sample_df, sample_id_col, drop_duplicated_samples
-    )
-    return sample_df
 
 
 def validate_celltype_columns(
     celltype_columns: List[str], sample_id_col: str, celltype_col: str, barcode_col: str
 ) -> None:
     """
-    Check if celltype dataframe contains all necessary columns
-    celltype_columns (List[str]): a list of columns in celltype annotation
+    Check if celltype DataFrame contains all necessary columns
+    celltype_columns (List[str]):
     reference_columns (Set[str]): a list of expected columns
+
+    Args:
+        celltype_columns (List[str]): a list of columns in celltype annotation
+        sample_id_col (str): a columns with sample id in annotation file
+        celltype_col (str): a columns with celltype label in annotation file
+        barcode_col (str): a columns with barcode sequence in annotation file
+
+    Raises:
+        InvalidColumnName: if any columns are missing
     """
     reference_columns = {sample_id_col, celltype_col, barcode_col}
     if reference_columns.difference(celltype_columns):
@@ -176,9 +174,13 @@ def validate_celltype_columns(
 
 def filter_nan_entries(celltypes: DataFrame, dropna: bool) -> None:
     """
-    Filters NaN values in celltypes dataframe for sample_id
-    celltypes (DataFrame): a dataframe with barcode/celltype annotation for sample_id
-    dropna (bool): if true drops NaN values from celltype annotation
+    Filters NaN values in celltypes DataFrame for sample_id
+    Args:
+        celltypes (DataFrame): a DataFrame with celltype annotation
+        dropna (bool): if true drops NaN values from celltype annotation
+
+    Raises:
+        DataError: if dropna=False and annotation file contains NaN value
     """
     nan_etries = celltypes.isna().values.any()
     if nan_etries:
@@ -192,17 +194,24 @@ def filter_nan_entries(celltypes: DataFrame, dropna: bool) -> None:
 
 
 def subsample_celltypes(
-    celltypes: DataFrame, samples: Set[str], sample_id_col: str
+    celltypes: DataFrame, samples: List[str], sample_id_col: str
 ) -> DataFrame:
     """
     Subsample samples from celltypes annotation
-    celltypes (DataFrame): a dataframe with barcode/celltype annotation for sample_id
-    samples (Set[str]): a set of samples from sample table
-    sample_id_col (str): a name for sample_id column in annotation file
+    Args:
+        celltypes (DataFrame): a DataFrame with celltype annotation
+        samples (List[str]): a list of samples from sample table
+        sample_id_col (str): a name for sample_id column in annotation file
+
+    Raises:
+        DataError: if there are no such such samples in the annotation file
+
+    Returns:
+        DataFrame: subsampled celltype annotation DataFrame
     """
     # check if all samples are in the celltype annotation DataFrame
     samples_in_annotation = celltypes[sample_id_col].unique().tolist()
-    difference = samples.difference(samples_in_annotation)
+    difference = set(samples).difference(samples_in_annotation)
     if difference:
         sample_string = ",".join(difference)
         raise DataError(
@@ -215,7 +224,7 @@ def subsample_celltypes(
 
 def read_celltype_annotation(
     filepath: str,
-    samples: Set[str],
+    samples: List[str],
     sample_id_col: str,
     celltype_col: str,
     barcode_col: str,
@@ -223,12 +232,16 @@ def read_celltype_annotation(
 ) -> DataFrame:
     """
     Read cell-type annotation file to pandas DataFrame
-    filepath (str): a path to celltype annotation file
-    samples (Set[str]): a set of samples from sample table
-    sample_id_col (str): a name for sample_id column in annotation file
-    celltype_col (str): a name for celltype column in annotation file
-    barcode_col (str): a name for barcode column in annotation file
-    dropna (bool): if true drops NaN values from celltype annotation
+    Args:
+        filepath (str): a path to celltype annotation file
+        samples (List[str]): a list of samples from sample table
+        sample_id_col (str): a name for sample_id column in annotation file
+        celltype_col (str): a name for celltype column in annotation file
+        barcode_col (str): a name for barcode column in annotation file
+        dropna (bool): if true drops NaN values from celltype annotation
+
+    Returns:
+        DataFrame: cell-type annotation for samples in sample list
     """
     # read chromsizes file
     celltypes = read_csv(filepath)
@@ -243,12 +256,163 @@ def read_celltype_annotation(
     return celltypes_subsample
 
 
+def read_barcode_metrics(
+    barcode_metrics_file: str, barcode_col: str, fragments_col: str
+) -> DataFrame:
+    """
+    Reads cellranger-arc output per_barcode_metrics.csv file
+    Args:
+        barcode_metrics_file (str): a path to per_barcode_metrics.csv file
+        barcode_col str: a name of the column with barcode names
+        fragments_col str: a name of the column with counts of fragments per barcode
+
+    Returns:
+        DataFrame: contains fragment number for each barcode
+    """
+    barcode_metrics = read_csv(
+        barcode_metrics_file, usecols=[barcode_col, fragments_col]
+    )
+    return barcode_metrics
+
+
+def get_sample_fragments_per_celltype(
+    sample: str,
+    barcode_metrics_df: DataFrame,
+    celltypes: DataFrame,
+    sample_id_col: str,
+    celltype_col: str,
+    barcode_col: str,
+    fragments_col: str,
+) -> DataFrame:
+    """
+    Generates a DataFrame of fragment number per celltype for sample
+    Args:
+        sample (str): _description_
+        barcode_metrics_df (DataFrame): a DataFrame with fragment number for each barcode
+        celltypes (DataFrame): a DataFrame with celltype annotation
+        sample_id_col (str): a name for sample_id column in annotation file
+        celltype_col (str): a name for celltype column in annotation file
+        barcode_col (str): a name for barcode column in annotation and barcode metrics files
+        fragments_col (str): a name for fragments number column in barcode metrics file
+
+    Returns:
+        DataFrame: contains a fragment number per celltype, shape=(n_celltypes, 1)
+    """
+    # subsample celltype DataFrame
+    celltypes_sub = celltypes[celltypes[sample_id_col] == sample].copy()
+    # join celltype DataFrames
+    joined_df = celltypes_sub.merge(
+        barcode_metrics_df, left_on=barcode_col, right_on=barcode_col
+    )
+    # calculate a total number of fragments per celltype
+    total_fragments = (
+        joined_df.groupby(celltype_col)
+        .aggregate({fragments_col: "sum"})
+        .rename({fragments_col: sample}, axis=1)
+    )
+    return total_fragments
+
+
+def get_fragment_counts(
+    sample_to_metrics: Dict[str, DataFrame],
+    celltypes: DataFrame,
+    sample_id_col: str,
+    celltype_col: str,
+    barcode_col: str,
+    fragments_col: str,
+) -> DataFrame:
+    """
+
+    Args:
+        sample_to_metrics (Dict[str, DataFrame]): a Dict with barcode metrics DataFrames for each sample
+        celltypes (DataFrame): a DataFrame with celltype annotation
+        sample_id_col (str): a name for sample_id column in annotation file
+        celltype_col (str): a name for celltype column in annotation file
+        barcode_col (str): a name for barcode column in annotation and barcode metrics files
+        fragments_col (str): a name for fragments number column in barcode metrics file
+
+    Returns:
+        DataFrame: a number of fragments per celltype for each sample, shape=(n_celltypes, n_samples)
+    """
+    # calculate total fragments per celltype for each sample
+    sample_fragments_per_celltype_list = [
+        get_sample_fragments_per_celltype(
+            sample,
+            barcode_metrics,
+            celltypes,
+            sample_id_col,
+            celltype_col,
+            barcode_col,
+            fragments_col,
+        )
+        for sample, barcode_metrics in sample_to_metrics.items()
+    ]
+    # convert to DataFrame
+    fragments_celltype_x_sample = (
+        concat(sample_fragments_per_celltype_list, axis=1).fillna(0).astype("int64")
+    )
+    return fragments_celltype_x_sample
+
+
+def get_fragments_per_sample(
+    sample_to_metrics: Dict[str, DataFrame], fragments_col: str
+) -> DataFrame:
+    """
+    Creates a DataFrame with total number of fragments for each sample
+    Args:
+        sample_to_metrics (Dict[str, DataFrame]): a Dict with barcode metrics DataFrames for each sample
+        fragments_col (str): a name for fragments number column in barcode metrics file
+
+    Returns:
+        DataFrame: a total number of fragments for each sample, shape=(n_samples, 1)
+    """
+    sample_fragments_list = [
+        metrics[fragments_col].sum() for sample, metrics in sample_to_metrics.items()
+    ]
+    fragments_per_sample_df = DataFrame(
+        sample_fragments_list, columns=["fragments"], index=sample_to_metrics.keys()
+    )
+    return fragments_per_sample_df
+
+
+def drop_zero_fragment_celltypes(
+    fragments_per_celltype: Series, celltypes: DataFrame, celltype_col: str
+) -> DataFrame:
+    """
+    Drops celltypes with zero fragments from celltype DataFrame
+    Args:
+        fragments_per_celltype (Series): a Series with total number of fragments per celltype
+        celltypes (DataFrame): a DataFrame with celltype annotation
+        celltype_col (str): a name for celltype column in annotation file
+
+    Returns:
+        DataFrame: filtered celltype annotation
+    """
+    # find celltypes with zero fragments
+    zero_fragment_celltypes = fragments_per_celltype[
+        fragments_per_celltype == 0
+    ].index.to_list()
+    if zero_fragment_celltypes:
+        celltype_string = ",".join(zero_fragment_celltypes)
+        # log this step
+        logging.warning(
+            f"No fragments for {celltype_string} celltypes. Dropping them from celltype annotation file..."
+        )
+        # drop the celltypes
+        zero_fragment_rows = celltypes[
+            celltypes[celltype_col].isin(zero_fragment_celltypes)
+        ].index
+        celltypes = celltypes.drop(zero_fragment_rows).copy()
+    return celltypes
+
+
 def split_celltypes(celltypes_df: DataFrame, celltype_col: str, output_dir: str):
     """
     Split celltype annotation in separate files for each celltype
-    celltypes_df (DataFrame): a dataframe with barcode/celltype annotation for sample_id
-    celltype_col (str): a name for celltype column in annotation file
-    output_dir (str): an output directory name
+    Args:
+        celltypes_df (DataFrame): a DataFrame with celltype annotation
+        celltype_col (str): a name for celltype column in annotation file
+        output_dir (str): an output directory name
     """
     unique_celltypes = celltypes_df[celltype_col].unique().tolist()
     for celltype in unique_celltypes:
@@ -269,31 +433,52 @@ def main():
     # set up logger
     setup_logging(args.logfile)
 
-    # read sample table and get sample list
-    sample_table = read_sample_table(
-        args.sample_table,
-        args.sample_id_col,
-        args.filedir_col,
-        args.drop_duplicated_samples,
-    )
-    samples = set(sample_table[args.sample_id_col])
+    # check if there are duplicates in sample list
+    validate_sample_list(args.sample_id)
 
     # read celltype annotation
     celltypes = read_celltype_annotation(
-        args.celltype_annotation,
-        samples,
+        filepath=args.celltype_annotation,
+        samples=args.sample_id,
+        sample_id_col=args.sample_id_col,
+        celltype_col=args.celltype_col,
+        barcode_col=args.barcode_col,
+        dropna=True,
+    )
+
+    # make a dictionary with barcode metrics DataFrames
+    sample_to_metrics_path = dict(zip(args.sample_id, args.barcode_metrics))
+    sample_to_metrics = {
+        sample: read_barcode_metrics(metrics_path, args.barcode_col, args.fragments_col)
+        for sample, metrics_path in sample_to_metrics_path.items()
+    }
+
+    # calculate fragment numbers
+    fragments_celltype_x_sample = get_fragment_counts(
+        sample_to_metrics,
+        celltypes,
         args.sample_id_col,
         args.celltype_col,
         args.barcode_col,
-        args.dropna,
+        args.fragments_col,
+    )
+    fragments_per_celltype = fragments_celltype_x_sample.sum(axis=1)
+    fragments_per_sample = get_fragments_per_sample(
+        sample_to_metrics, args.fragments_col
     )
 
-    # split celltype annotation in separate files
+    # fillter celltypes with zero fragments and split celltype annotation in separate files
     os.mkdir(args.output_dir)
+    celltypes = drop_zero_fragment_celltypes(
+        fragments_per_celltype, celltypes, args.celltype_col
+    )
     split_celltypes(celltypes, args.celltype_col, args.output_dir)
 
-    # save filtered sample table
-    sample_table.to_csv(args.filtered_sample_table, index=False)
+    # save fragment counts
+    fragments_celltype_x_sample.to_csv(args.fragments_celltype_x_sample)
+    fragments_per_celltype.index = fragments_per_celltype.index.str.replace(' ', '_')
+    fragments_per_celltype.to_csv(args.fragments_per_celltype, header=False)
+    fragments_per_sample.to_csv(args.fragments_per_sample, header=False)
 
 
 if __name__ == "__main__":
